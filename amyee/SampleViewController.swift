5cf6aadc66a737a835b53a01bb5111370502c468
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-04-28 11:51:51 +0900

    add: sample controller and main strory board

---
+++ amyee/SampleViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SampleViewController.swift
+//  amyee
+//
+//  Created by kazuma on 2018/04/28.
+//  Copyright © 2018年 kazuma. All rights reserved.
+//
+
+import UIKit
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
